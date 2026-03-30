cc7ee03ac821df8e835af86156a9e9cec53d0205
Fran4end <francyd04@gmail.com>
2026-03-30 16:39:28 +0200

    add button colors and deafault padding as costants

---
+++ app/src/main/java/it/unipd/francesco_dotoli/simon_game/constants.kt
@@ -0,0 +1,15 @@
+package it.unipd.francesco_dotoli.simon_game
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+val defaultPadding = 24.dp
+
+val colorsList = listOf<Color>(
+    Color(0xfff02626),
+    Color(0xFF0CB808),
+    Color(0xFF2196F3),
+    Color(0xFFE758D8),
+    Color(0xfff0c23a),
+    Color(0xFF00BCD4)
+    )
